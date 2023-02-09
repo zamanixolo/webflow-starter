ed5f88aac97ed393110c7e7fd2aeb29746f0a076
zamanixolo <zamani.xolo@gmail.com>
2023-02-09 11:11:53 +0200

    User name change
    Changed user name from Zamani Xolo to Solomon Giga

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { greetUser } from '$utils/greet';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'Zamani Xolo';
+  const name = 'Solomon Giga';
   greetUser(name);
 });
