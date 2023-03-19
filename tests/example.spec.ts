6d8e08150356ce8b1fc994bf075c2e51f789ff8d
zamanixolo <zamani.xolo@gmail.com>
2023-03-19 21:34:46 +0200

    update

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,5 @@
-import { test, expect } from '@playwright/test';
 import type { Page } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 /**
  * These are some demo tests to showcase Playwright.
